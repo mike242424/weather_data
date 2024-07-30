3396f57014c2fdb44c658611477f4250d1633154
Michael Byers <mikebyers24@gmail.com>
2024-07-30 08:42:27 -0400

    refactored getting average temp using pandas built in series.mean method.

--- main.py
+++ main.py
@@ -9,18 +9,7 @@
 #     print(temperatures)
 import pandas
 
-
-def get_average_temp(temp_list):
-    total = 0
-    index = 0
-    for temp in temp_list:
-        total += temp
-        index += 1
-
-    return round(total / index, 2)
-
-
 data = pandas.read_csv('./weather_data.csv')
-print(get_average_temp(data['temp'].tolist()))
+print(round(data['temp'].mean(), 2))
 
 
