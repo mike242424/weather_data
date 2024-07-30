f846757f9595c5b0909b34e61a3e5758724bdfeb
Michael Byers <mikebyers24@gmail.com>
2024-07-30 08:40:41 -0400

    getting average temp.

--- main.py
+++ main.py
@@ -7,7 +7,20 @@
 #         if row[1] != 'temp':
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
-
 import pandas
+
+
+def get_average_temp(temp_list):
+    total = 0
+    index = 0
+    for temp in temp_list:
+        total += temp
+        index += 1
+
+    return round(total / index, 2)
+
+
 data = pandas.read_csv('./weather_data.csv')
-print(data['temp'])
+print(get_average_temp(data['temp'].tolist()))
+
+
